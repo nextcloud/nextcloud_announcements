ce4bc7bb1bfee958365ebc9db9bd74ac3cd32f6a
Joas Schilling <coding@schilljs.com>
2019-09-11 11:36:29 +0200

    Allow to directly disable the nextcloud announcements from the notification
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Notification/Notifier.php
+++ lib/Notification/Notifier.php
@@ -24,6 +24,7 @@
 namespace OCA\NextcloudAnnouncements\Notification;
 
 
+use OCP\IGroupManager;
 use OCP\IURLGenerator;
 use OCP\L10N\IFactory;
 use OCP\Notification\INotification;
@@ -35,22 +36,21 @@ class Notifier implements INotifier {
 
 	/** @var string */
 	protected $appName;
-
 	/** @var IFactory */
 	protected $l10nFactory;
-
 	/** @var IURLGenerator */
 	protected $url;
+	/** @var IGroupManager */
+	protected $groupManager;
 
-	/**
-	 * @param string $appName
-	 * @param IFactory $l10nFactory
-	 * @param IURLGenerator $url
-	 */
-	public function __construct($appName, IFactory $l10nFactory, IURLGenerator $url) {
+	public function __construct(string $appName,
+								IFactory $l10nFactory,
+								IURLGenerator $url,
+								IGroupManager $groupManager) {
 		$this->appName = $appName;
 		$this->l10nFactory = $l10nFactory;
 		$this->url = $url;
+		$this->groupManager = $groupManager;
 	}
 
 	/**
@@ -74,6 +74,15 @@ class Notifier implements INotifier {
 				$notification->setParsedSubject($l->t('Nextcloud announcement'))
 					->setParsedMessage($parameters[0])
 					->setIcon($this->url->getAbsoluteURL($this->url->imagePath($this->appName, 'app-dark.svg')));
+
+				$isAdmin = $this->groupManager->isAdmin($notification->getUser());
+				if ($isAdmin) {
+					$action = $notification->createAction();
+					$action->setParsedLabel($l->t('Disable announcements'))
+						->setLink($this->url->linkToOCSRouteAbsolute('provisioning_api.AppsController.disable', ['app' => 'nextcloud_announcements']), 'DELETE')
+						->setPrimary(false);
+					$notification->addParsedAction($action);
+				}
 
 				return $notification;
 
